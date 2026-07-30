6870e0e8b39a336e976a77ba0515ba497ac2bcea
allusional <chenallenbms@gmail.com>
2026-07-30 17:21:15 -0400

    Revert "Port CBTRN02C (POSTTRAN) COBOL batch to Java with tests"

--- java/carddemo-posttran/src/main/java/com/carddemo/posttran/RejectWriter.java
+++
@@ -1,7 +0,0 @@
-package com.carddemo.posttran;
-
-/** DALYREJS-FILE output (2500-WRITE-REJECT-REC). */
-public interface RejectWriter {
-
-    void write(RejectRecord reject);
-}
--- java/carddemo-posttran/src/main/java/com/carddemo/posttran/XrefRepository.java
+++
@@ -1,9 +0,0 @@
-package com.carddemo.posttran;
-
-import java.util.Optional;
-
-/** XREF-FILE, keyed by card number (1500-A-LOOKUP-XREF). */
-public interface XrefRepository {
-
-    Optional<CardXref> findByCardNum(String cardNum);
-}
